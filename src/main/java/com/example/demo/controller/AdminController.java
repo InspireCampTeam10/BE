678344c3a738874b48fb93cda5d29de182a3471e
jeongyeon0208 <githubb0208@gmail.com>
2025-02-24 13:47:18 +0900

    Refactor : admin 기능 AdminController로 이동

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,15 +1,40 @@
 package com.example.demo.controller;
 
+import com.example.demo.global.apipayLoad.ApiResponse;
+import com.example.demo.service.footballService.FootballService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@ResponseBody
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping("/admin")
+    private final FootballService footballService;
+
+    @GetMapping("")
     public String adminP(){
         return "Admin Controller";
+    }
+
+
+    @PostMapping("/football/init")
+    public ApiResponse<?> registerLeagueAndTeamInit() throws JsonProcessingException {
+        footballService.registerLeagueAndTeamStanding();
+        return ApiResponse.onSuccess("초기 리그 순위 및 팀 통계 정보 등록 성공");
+    }
+
+    @PatchMapping("/football/update")
+    public ApiResponse<?> updateStanding() throws JsonProcessingException {
+        footballService.updateTeamStatisticsAndStanding();
+        return ApiResponse.onSuccess("순위 및 통계 정보 최신화 성공");
+    }
+
+    @PatchMapping("/football/team/init")
+    public ApiResponse<?> updateTeamInitInfo() throws JsonProcessingException {
+        footballService.updateTeamInitInfo();
+        return ApiResponse.onSuccess("팀 상세 정보 등록 성공");
     }
 }
--- src/main/java/com/example/demo/controller/FootballController.java
+++ src/main/java/com/example/demo/controller/FootballController.java
@@ -20,21 +20,5 @@ public class FootballController {
         return ApiResponse.onSuccess(homeResponse);
     }
 
-    @PostMapping("/init")
-    public ApiResponse<?> registerLeagueAndTeamInit() throws JsonProcessingException {
-        footballService.registerLeagueAndTeamStanding();
-        return ApiResponse.onSuccess("초기 리그 순위 및 팀 통계 정보 등록 성공");
-    }
 
-    @PatchMapping("/update")
-    public ApiResponse<?> updateStanding() throws JsonProcessingException {
-        footballService.updateTeamStatisticsAndStanding();
-        return ApiResponse.onSuccess("순위 및 통계 정보 최신화 성공");
-    }
-
-    @PatchMapping("/team/init")
-    public ApiResponse<?> updateTeamInitInfo() throws JsonProcessingException {
-        footballService.updateTeamInitInfo();
-        return ApiResponse.onSuccess("팀 상세 정보 등록 성공");
-    }
 }
